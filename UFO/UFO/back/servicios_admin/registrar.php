be24debc229c98a94cd037dd1eb08352f3528fe6
ufopress <ufopress@hotmail.com>
2024-10-02 17:52:05 -0300

    Update registrar.php

--- UFO/UFO/back/servicios_admin/registrar.php
+++ UFO/UFO/back/servicios_admin/registrar.php
@@ -1,7 +1,7 @@
 <?php
 include "../controladores/conectar.php";
 
-$nombreuser= $_POST['nombre'];
+$nombreuser = $_POST['nombre'];
 $email = $_POST['email'];
 $contrasenia = $_POST['contrasenia'];
 $telefono = $_POST['telefono'];
@@ -9,10 +9,9 @@ $nacionalidad = $_POST['nacionalidad'];
 $añonacimiento = $_POST['añonacimiento'];
 
 //try{
-    $sql = "INSERT INTO `cliente` (`IdUsuario`,`NombreUser`, `Email`, `Contrasenia`, `TipoUsuario`, `NroTelefono`, `Nacionalidad`, `AñoNacimiento`) VALUES
-     ('1', '$nombreuser', '$email', '$contrasenia', '1', '$telefono', '$nacionalidad', '$añonacimiento' )";
-    $sentencia=$conexion->prepare($sql);
-    $sentencia->execute();
+$sql = "INSERT INTO `cliente` (`IdUsuario`,`NombreUser`, `Email`, `Contrasenia`, `TipoUsuario`, `NroTelefono`, `Nacionalidad`, `AñoNacimiento`) VALUES ('1', '$nombreuser', '$email', '$contrasenia', '1', '$telefono', '$nacionalidad', '$añonacimiento' )";
+$sentencia = $conexion->prepare($sql);
+$sentencia->execute();
 //    header("location:../vistas/admin.html");
 //}catch(PDOException $a){
 //    header("location:error.php");
