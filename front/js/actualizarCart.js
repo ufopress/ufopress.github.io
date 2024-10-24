ead793ec3ce3cb5d898c65f2406f2b10be8930e4
brianfernandezmartinez <briandanielm2@outlook.es>
2024-10-24 04:30:08 -0300

    Nueva funcionalidad: se actualiza carrito en el DOM

---
+++ front/js/actualizarCart.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Llama a la función para mostrar el total de ítems en el carrito cuando la página cargue
+    actualizarContadorCarrito();
+});
